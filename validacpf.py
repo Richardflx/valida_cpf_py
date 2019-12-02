1f2332305e5a264a18ab74b7c037936a4f6553c5
Richardflx <rfelixr@outlook.com>
2019-12-02 11:39:29 -0300

    Adicionado a Opção: Continuar ou Sair

--- validacpf.py
+++ validacpf.py
@@ -1,4 +1,5 @@
 while True:
+    print('Bem-vindo ao Validador de CPF!')
     cpf = input('Digite seu CPF: ')
     if cpf.isnumeric() and len(cpf) != 11 or not cpf.isnumeric():
         print('Por favor, digite apenas os 11 números.')
@@ -23,3 +24,27 @@ while True:
         print('CPF Válido')
     else:
         print('CPF Inválido')
+    print("-"*10)
+
+    def SAIR():
+        while True:
+            continua = str(input("""Deseja verificar outro CPF?
+Digite 'S' para continuar.
+Digite 'N' para sair. 
+""").upper())
+            if continua == 'S' or continua == 'N':
+                break
+            else:
+                print('Opção inválida')
+                continue
+        if continua == 'S':            
+            return '1'
+        if continua == 'N':
+            return '2'
+    
+    sair = SAIR()
+    if sair == '1':
+        print('-'*10)
+        continue
+    else:
+        break
